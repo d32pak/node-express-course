709300c7954fb558d94d78618c27876b523c57f2
Deepak Manickam <deepak.manickam@aon.com>
2020-12-01 20:48:41 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+// Setting up express
 const express = require('express');
 const app = express();
 const port = 8000;
+// Setting body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const mockUserData=[
     {name:'Mark'},
@@ -11,6 +15,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'Successfully got users. Nice!',
+		users: mockUserData
+	})
+})
+
 app.get('/users/:id', function(req,res){
     console.log(req.params.id);
     res.json({
@@ -18,6 +30,26 @@ app.get('/users/:id', function(req,res){
         message: 'Got one user!',
         users: req.params.id
     })
+})
+
+app.post('/login', function(req,res) {
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const chkName="billyTheKid";
+     const chkPass="superSecret";
+     if(username===chkName && password===chkPass) {
+        res.json({
+            success: true,
+            message: 'Login Success!',
+            token: 'Encryption here'
+        })
+     } else {
+         res.json({
+             success: false,
+             message: 'Login Failed. Want to reset credentials?'
+         })
+     }
 })
 
 app.listen(port,function(){
